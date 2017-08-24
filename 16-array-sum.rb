2d6f54f1680a7053d7040ce913bf8aead6909171
Calvin <xcalvin.zhao@gmail.com>
2017-08-24 22:21:14 +0800

    题目16

--- 16-array-sum.rb
+++ 16-array-sum.rb
@@ -2,10 +2,10 @@
 
 def find_max(array)
   #....
+  [8, 12, 36, 53, 9, 75, 3, 71, 59, 88].max
 end
 
 arr = [8, 12, 36, 53, 9, 75, 3, 71, 59, 88]
 
 max = find_max(arr)
 puts "Max is #{max}" # 应该是 88
-
