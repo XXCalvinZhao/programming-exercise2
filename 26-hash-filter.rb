2cb1f17c2d6e8d4c2fc53a001c498d1fccd76b99
Calvin <xcalvin.zhao@gmail.com>
2017-08-25 22:56:41 +0800

    题目26

--- 26-hash-filter.rb
+++ 26-hash-filter.rb
@@ -10,7 +10,17 @@ arr = [
 
 # ....
 
-puts "所有成年人，并由小到大: _________"
+def filter(arr)
+  result_arr = []
+  arr.each do |h|
+    if h["age"] >= 18
+      result_arr.push(h)
+    end
+  end
+  result_arr.sort_by{ |i| i["age"] }
+end
+
+puts "所有成年人，并由小到大: #{filter(arr)}"
 
 # 答案应该是
 #[
