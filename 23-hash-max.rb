6617237ff045bcc407608a0627f717dcce0ef189
Calvin <xcalvin.zhao@gmail.com>
2017-08-25 22:21:22 +0800

    题目23

--- 23-hash-max.rb
+++ 23-hash-max.rb
@@ -2,6 +2,9 @@
 
 def find_max(hash)
   # ...
+  hash.each do |key, value|
+    return key if value == hash.values.max
+  end
 end
 
 h = {
@@ -15,5 +18,3 @@ h = {
 answer = find_max(h)
 
 puts "有最大 value 的是 #{answer}" # 应该是 d
-
-
